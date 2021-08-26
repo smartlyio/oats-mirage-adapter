0a9ff3b257b822953e3c3f85eec8f514dede8398
sakari <sakariij@gmail.com>
2021-08-26 10:26:47 +0300

    deprecated

--- test-app/README.md
+++
@@ -1,7 +0,0 @@
-# Getting Started with Oats test app
-
-```
-yarn
-yarn oas   # to generate the code from the openapi spec
-yarn start # to start the test app in http://localhost:3000
-```
--- test-app/driver.js
+++
@@ -1,9 +0,0 @@
-const { driver } = require('@smartlyio/oats');
-
-driver.generate({
-  generatedValueClassFile: './src/types.generated.ts',
-  generatedServerFile: './src/server.generated.ts',
-  header: '/* tslint:disable variable-name only-arrow-functions*/',
-  openapiFilePath: './example.yml',
-  resolve: driver.compose(driver.generateFile(), driver.localResolve),
-});
